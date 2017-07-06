db4bf1659c0b44ca966bd71cdd17eda53616336a
Hack Reactor Student <student@hackreactor.com>
2017-07-05 20:01:51 -0700

    Functional-Shared Stack Complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {'index': 0, 'storage': {}};
+  stack.push = stackMethods.push;
+  stack.pop = stackMethods.pop;
+  stack.size = stackMethods.size;
+  
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    this.storage[this.index.valueOf()] = value;
+    this.index++;
+  },
+  pop: function () {
+    var temp = this.storage[this.index.valueOf() - 1];
+    delete this.storage[this.index.valueOf() - 1];
+    if (this.index > 0) {
+      this.index--;
+    }
+    return temp;
+  },
+  size: function () {
+    return this.index;
+  }
+};
 
 

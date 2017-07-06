b1f546d121f45f63312c7ec8dabf9f39ac945986
Hack Reactor Student <student@hackreactor.com>
2017-07-06 16:03:05 -0700

    graph and part of tree complete

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,36 +1,77 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
+  this.edges = [];
+  this.nodes = [];
+  return graph;
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  
+  return this.nodes.includes(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i] === node) {
+      this.nodes.splice(i, 1);
+    } 
+  }
+  this.removeEdge(node, node);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (!this.edges.includes([fromNode, toNode].toString())) {
+    return this.edges.includes([toNode, fromNode].toString());
+  } else {
+    return true;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges.push([fromNode, toNode].toString());
+  console.log(this.edges);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (fromNode === toNode) {
+    //delete all edges containing node
+    for (var i = 0; i < this.edges.length; i++) {
+      var temp = this.edges[i].split(',');
+      //console.log(temp);
+      if (temp.includes(fromNode.toString())) {
+        //console.log(this.edges);
+        this.edges.splice(i, 1);
+        //console.log(this.edges);
+      }
+    }
+    
+  } else {
+    //delete edge with both nodes
+    for (var i = 0; i < this.edges.length; i++) {
+      if (this.edges[i] === [fromNode, toNode].toString() || this.edges[i] === [toNode, fromNode].toString() ) {
+        this.edges.splice(i, 1);
+      }
+    }
+  }
+  
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  this.nodes.forEach(cb);
 };
+
+var graph = new Graph();
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  
 
   return newTree;
 };
@@ -11,9 +12,55 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
+/*
+            undefined 3
+        a      b       c    
+      1  2     3     4  5  6
+            
+
+*/
 treeMethods.contains = function(target) {
+  for (var index = 0; index < this.children.length; index++) {
+    if (this.children[index].value === target) {
+      return true;
+    } else if (this.children[index].children.length > 0) {
+      //console.log('grandchildren');
+      return this.children[index].contains(target);
+    } else {
+      return false;
+    }
+  }
+  
+  
+  
+  
+  //return this.children.includes(target);
+  /*if (flag === undefined) {
+    flag = false;
+  }
+  if (this.value === target) {
+    flag = true;
+    console.log(flag);
+    return flag;
+  }
+  if (this.value !== target && this.children.length > 0) {
+    //this.children.forEach(this.contains(target, flag));
+    for (var i = 0; i < this['children'].length; i++) {
+      console.log(flag);
+      this.children[i].contains(target, flag);
+    }
+    console.log('for each');
+    console.log(flag);
+    return flag;
+  }
+  if (this.value !== target && this.children.length === 0) {
+    console.log('not found');
+    console.log(flag);
+    return flag;
+  } */
 };
 
 

1494894608d8a0ddc36f66b623c50e564e361be7
Hack Reactor Student <student@hackreactor.com>
2017-07-08 17:22:44 -0700

    BST breadthFirstLog

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -37,6 +37,21 @@ describe('binarySearchTree', function() {
     expect(array).to.eql([5, 2, 3]);
   });
 
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function() {
+    //var array = [];
+    //var func = function(value) { array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(9);
+    //binarySearchTree.breadthFirstLog(func);
+    expect(binarySearchTree.breadthFirstLog()).to.eql([5, 2, 6, 1, 3, 8, 4, 7, 9]);
+  });
+
   it('should ignore duplicate values', function() {
     var array = [];
     var func = function(value) { array.push(value); };
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -6,20 +6,24 @@ var BinarySearchTree = function(value) {
   binaryTree.insert = binaryTreeMethods.insert;
   binaryTree.contains = binaryTreeMethods.contains;
   binaryTree.depthFirstLog = binaryTreeMethods.depthFirstLog;
-
+  binaryTree.breadthFirstLog = binaryTreeMethods.breadthFirstLog;
   return binaryTree;
 };
 
 var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function (value) {
+  //var depth = 0;
+  //var buildTree = function (value, depth) {
   if (this.value === undefined) {
     this.value = value;
+    //this.depth = depth
   }
   if (value < this.value) {
     if (this.left === undefined) {
       this.left = BinarySearchTree(value);
     } else {
+      //depth++
       this.left.insert(value);
     }
   }
@@ -27,6 +31,7 @@ binaryTreeMethods.insert = function (value) {
     if (this.right === undefined) {
       this.right = BinarySearchTree(value);
     } else {
+      //depth++
       this.right.insert(value);
     }
   }
@@ -76,6 +81,33 @@ binaryTreeMethods.depthFirstLog = function (callback) {
   
 };
 
+binaryTreeMethods.breadthFirstLog = function () {
+  results = [];
+  results.push(this);
+  counter = 0;
+  var breadthTraversal = function (node) {
+    if (typeof(node) === 'object') {
+      if (node.left !== undefined) {
+        results.push(node.left);
+      }
+      if (node.right !== undefined) {
+        results.push(node.right);
+      }
+      counter++;
+      breadthTraversal(results[counter]);
+    } else {
+      return;
+    }
+    
+  };
+  
+  breadthTraversal(this);
+  results = results.map(function(element) {
+    return element.value;
+  });
+  
+  return results;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
